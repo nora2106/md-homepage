98619e135718adf4395eed06e445a855424ab2c7
nora2106 <noraklinger@outlook.de>
2025-04-07 15:34:57 +0200

    optimize large images

--- app/components/03_organisms/Hero/Hero.js
+++ app/components/03_organisms/Hero/Hero.js
@@ -40,21 +40,21 @@ export const Hero = (props) => {
     return (
         <div ref={heroRef} className={styles.hero}>
             <div className={styles.imageWrapper}>
-                <Image className={styles.topFlowers} width="500" height="600" src='/img/top-flowers.png' alt=''/>
+                <Image loading="eager" className={styles.topFlowers} width="500" height="600" src='/img/top-flowers.webp' alt=''/>
                 <div className={styles.playerImageWrapper}>
                     <div className={styles.playerImgs}>
-                        <Image className={styles.player} width="500" height="600" src='/img/player1.png' alt=''/>
-                        <Image className={styles.bird} width="400" height="500" src='/img/bird.png' alt=''/>
-                        <Image className={styles.flower} width="200" height="200" src='/img/flower.png' alt=''/>
+                        <Image loading="eager" className={styles.player} width="500" height="600" src='/img/player1.webp' alt=''/>
+                        <Image loading="eager" className={styles.bird} width="400" height="500" src='/img/bird.png' alt=''/>
+                        <Image loading="eager" className={styles.flower} width="200" height="200" src='/img/flower.png' alt=''/>
                     </div>
                 </div>
                 <div className={styles.plants}>
-                <Image className={styles.leftPlants} width="500" height="600" src='/img/left-plant.png' alt=''/>
+                <Image loading="eager" className={styles.leftPlants} width="500" height="600" src='/img/left-plant.png' alt=''/>
                     <div className={styles.middlePlants}>
-                        <Image className={styles.rightPlantThird} width="500" height="600" src='/img/right-plant2.png' alt=''/>
-                        <Image className={styles.rightPlantSecond} width="500" height="600" src='/img/right-plant2.png' alt=''/>
+                        <Image loading="eager" className={styles.rightPlantThird} width="500" height="600" src='/img/right-plant2.png' alt=''/>
+                        <Image loading="eager" className={styles.rightPlantSecond} width="500" height="600" src='/img/right-plant2.png' alt=''/>
                     </div>
-                    <Image className={styles.rightPlant} width="500" height="600" src='/img/right-plant.png' alt=''/>
+                    <Image loading="eager" className={styles.rightPlant} width="500" height="600" src='/img/right-plant.png' alt=''/>
                 </div>
                 <p className={styles.annotation}>© Bild: {imgCredit}</p>
             </div>
