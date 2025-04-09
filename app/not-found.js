41d002a1f2ed7a0b1be2e8f31023777d7186a5ee
nora2106 <noraklinger@outlook.de>
2025-04-09 12:57:51 +0200

    style 404 page

--- app/not-found.js
+++ app/not-found.js
@@ -1,9 +1,13 @@
-//@todo style 404 page
-export default function Custom404() {
-    return (
-        <body>
-            <h1>404 - Page Not Found</h1>
-        </body>
-    )
+import NotFound from "@/app/(layout)/not-found";
 
+export const metadata = {
+    title: "404 - Nicht gefunden",
+    robots: {
+        index: false,
+        follow: false,
+    },
+}
+
+export default function Custom404() {
+    return <NotFound/>
 }
